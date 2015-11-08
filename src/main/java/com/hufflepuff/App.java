eb358a1dff1db62ecc415fd479d46a9fd5069571
Lucas Farre <lucasfarre@gmail.com>
2015-11-08 15:05:11 -0300

    Se agrego el parser de args

--- src/main/java/com/hufflepuff/App.java
+++ src/main/java/com/hufflepuff/App.java
@@ -23,4 +23,36 @@ public class App
 //        Movie m = gson.fromJson(json, classOfT)
         System.out.println(gson.toJson(movies));
     }
+
+    private static QueryData parseArgs(String[] args) {
+        int n = -1;
+        int tope = -1;
+        String path = null;
+        for(String arg: args) {
+            String[] argSplitted = arg.split("=");
+            String argName = argSplitted[0].toLowerCase();
+            String argValue = argSplitted[1];
+            if(argName.equals("n")) {
+                n = Integer.parseInt(argValue);
+            } else if(argName.equals("tope")) {
+                tope = Integer.parseInt(argValue);
+            } else if(argName.equals("path")) {
+                path = argValue;
+            }
+        }
+        return new QueryData(n, tope, path);
+    }
+
+    private static class QueryData {
+        int n;
+        int tope;
+        String path;
+
+        public QueryData(int n, int tope, String path) {
+            this.n = n;
+            this.tope = tope;
+            this.path = path;
+        }
+    }
+
 }
