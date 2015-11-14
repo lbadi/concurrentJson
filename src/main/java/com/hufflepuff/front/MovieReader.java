7faeaf19812e939fdcda55f568ed68e421dc5360
Lucas Farre <lucasfarre@gmail.com>
2015-11-14 14:41:11 -0300

    Se hizo mas eficiente la lectura del archivo

--- src/main/java/com/hufflepuff/front/MovieReader.java
+++ src/main/java/com/hufflepuff/front/MovieReader.java
@@ -2,12 +2,14 @@ package com.hufflepuff.front;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 import com.hazelcast.core.IMap;
 import com.hufflepuff.FileReader;
 import com.hufflepuff.domain.Movie;
 import com.hufflepuff.util.Timestamper;
 
 import javax.xml.transform.sax.SAXSource;
+import java.io.FileNotFoundException;
 import java.lang.reflect.Type;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -19,16 +21,21 @@ public class MovieReader {
     public static void readMovies(IMap<String, Movie> movieMap, String path) {
         Gson gson = new Gson();
         FileReader fileReader = new FileReader();
-        System.out.println("Inicio de la lectura del archivo. " + Timestamper.getTime());
-        String file = fileReader.read(path);
-        System.out.println("Fin de lectura del archivo. " + Timestamper.getTime());
-        Type movieListType = new TypeToken<List<Movie>>(){}.getType();
-        List<Movie> movieList = gson.fromJson(file,movieListType);
-        movieList.forEach(movie -> {
-        	if(movie.getType().equals("movie")){
-        		movieMap.set(movie.getImdbID(), movie);
-        	}
-        });
+        JsonReader reader;
+        try {
+            System.out.println("Inicio de la lectura del archivo. " + Timestamper.getTime());
+            reader = new JsonReader(new java.io.FileReader(path));
+            Type movieListType = new TypeToken<List<Movie>>(){}.getType();
+            List<Movie> movies = gson.fromJson(reader, movieListType);
+            movies.forEach(movie -> {
+                if (movie.getType().equals("movie")) {
+                    movieMap.set(movie.getImdbID(), movie);
+                }
+            });
+            System.out.println("Fin de lectura del archivo. " + Timestamper.getTime());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
 }
